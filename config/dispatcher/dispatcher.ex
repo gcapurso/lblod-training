fc2f0b0dd534fea41b1d1e42fc33bc83f8a3e54e
Gianna <giovanna.capurso@gmail.com>
2021-01-14 16:15:37 +0100

    first commit

---
+++ config/dispatcher/dispatcher.ex
@@ -0,0 +1,41 @@
+defmodule Dispatcher do
+  use Plug.Router
+
+  def start(_argv) do
+    port = 80
+    IO.puts "Starting Plug with Cowboy on port #{port}"
+    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
+    :timer.sleep(:infinity)
+  end
+
+  plug Plug.Logger
+  plug :match
+  plug :dispatch
+
+  # In order to forward the 'themes' resource to the
+  # resource service, use the following forward rule.
+  #
+  # docker-compose stop; docker-compose rm; docker-compose up
+  # after altering this file.
+  #
+  # match "/themes/*path" do
+  #   Proxy.forward conn, path, "http://resource/themes/"
+  # end
+
+  # match "/themes/*path" do
+  #   Proxy.forward conn, path, "http://resource/themes/"
+  # end
+  
+  match "/books/*path" do
+    Proxy.forward conn, path, "http://resource/books/"
+  end
+
+  match "/authors/*path" do
+    Proxy.forward conn, path, "http://resource/authors/"
+  end
+
+  match _ do
+    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  end
+
+end
