9dd6a8b90556551d48c51017bfebba2d1b93793f
Gianna <giovanna.capurso@gmail.com>
2021-01-27 13:55:12 +0100

    Test

--- books/app/models/book.js
+++ books/app/models/book.js
@@ -3,5 +3,5 @@ import Model, {attr, belongsTo} from '@ember-data/model';
 export default class BookModel extends Model {
   @attr('string') title;
   @attr('string') isbn;
-  @belongsTo('author') author;
+  @belongsTo('author', { inverse: 'books'}) author;
 }
